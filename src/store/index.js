2f70b4bbb548df6a1fead25f6632a79c264d21cc
m1rstan1978 <m1rstan@mail.ru>
2023-11-11 12:11:40 +0300

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ export default createStore({
   state: {
     userName:null,
     userImageSrc:null,
-    urlPage:'http://localhost:8000/'
+    urlPage:'https://6bp98ls3-8000.euw.devtunnels.ms/'
   },
   getters: {
   },
